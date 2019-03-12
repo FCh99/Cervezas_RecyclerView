bea4473fa151181a202016341f82563230d9886f
Fausto Checa <fausto.checa@gmail.com>
2019-03-12 17:04:10 +0100

    Third

--- app/src/main/java/udemy/fausto/com/ej_4_activity2/Cerveza.kt
+++ app/src/main/java/udemy/fausto/com/ej_4_activity2/Cerveza.kt
@@ -3,7 +3,7 @@ package udemy.fausto.com.ej_4_activity2
 
 import java.util.*
 
-data class Cerveza (val id: Int, val precio: Double, val nombre: String, val imagen: String, val fechaFabricacion: String, val alcohol: String, val envase: String )
+data class Cerveza (val id: Int = 1, val precio: Double = 0.00, val nombre: String = "", val imagen: String = "", val fechaFabricacion: String = "1/1/1111", val alcohol: String = "No", val envase: String = "Ninguno" )
 
 
 
--- app/src/main/java/udemy/fausto/com/ej_4_activity2/MainActivity.kt
+++ app/src/main/java/udemy/fausto/com/ej_4_activity2/MainActivity.kt
@@ -1,15 +1,10 @@
 package udemy.fausto.com.ej_4_activity2
 
-import android.content.Context
-import android.content.Context.*
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.annotation.IntegerRes
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 import kotlin.collections.ArrayList
 
 /*
@@ -19,20 +14,17 @@ Android Kotlin app: el usuaro da de alta sus cervezas favoritas en una Activity,
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
-
     var cervezas = ArrayList<Cerveza>()
     var spinner: Spinner? = null
     var checkBox:CheckBox? = null
-    var radioGroup:RadioGroup? = null
 
-
+    var selectedEnvase = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         checkBox = this.checkBoxAlcohol
-        radioGroup = this.radioGroup1
         spinner = this.spinner2
 
         if (spinner != null) {
@@ -46,38 +38,48 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
 
         button3.setOnClickListener {
-            // recoger variables de la Actividad
-
-            var id = idEditText.text.toString().toInt()
-            var precio = precioEditText.text.toString().toDouble()
-            var nombre = nombreEditText.text.toString()
-            var imagen = spinner!!.getSelectedItem().toString()
-
-            var fechaDeFabricacion = editTextDate.text.toString()
-            var alcohol = checkAlcohol(checkBox!!)
-            var envase = typeOfEnvase(radioGroup!!) // TODO Problem ¡¡
-
-            var cerveza = Cerveza(id, precio, nombre, imagen, fechaDeFabricacion, alcohol, envase)
-
-            cervezas.add(cerveza)
-
-            println("-----------")
-            println(cervezas)
+            inicializarArrayCervezas()
 
 
         }
 
         }
-    override fun onNothingSelected(parent: AdapterView<*>?) {
 
-        println("Nothing selected")
+
+
+    // -------------   FUNCTIONS -----------------------
+
+    // recoge los datos del formulario y crea un array de Cervezas
+    fun inicializarArrayCervezas () {
+        var id: Int = 0
+        if (idEditText.text.isNotEmpty()) id = idEditText.text.toString().toInt()
+
+        var precio: Double = 0.00
+        if(precioEditText.text.isNotEmpty()) precio = precioEditText.text.toString().toDouble()
+
+        var nombre: String = ""
+        if(nombreEditText.text.isNotEmpty()) nombre = nombreEditText.text.toString()
+
+        var imagen: String = ""
+        if(spinner!!.getSelectedItem().toString().isNotEmpty()) imagen = spinner!!.getSelectedItem().toString()
+
+        var fechaDeFabricacion: String = "1/1/1111"
+        if (editTextDate.text.toString().isNotEmpty()) fechaDeFabricacion = editTextDate.text.toString()
+
+        var alcohol = "No"
+        if (checkAlcohol(checkBox!!).isNotEmpty()) alcohol = checkAlcohol(checkBox!!)
+
+        var envase = ""
+        if (selectedEnvase.isNotEmpty()) envase = selectedEnvase
+
+        var cerveza = Cerveza(id, precio, nombre, imagen, fechaDeFabricacion, alcohol, envase)
+        cervezas.add(cerveza)
+
+        println("-----------")
+        println(cervezas)
     }
 
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
 
-        println("Selected")
-
-    }
 
     fun checkAlcohol(mycheck: CheckBox): String {
         var alcoholSi = ""
@@ -85,19 +87,28 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         return alcoholSi
     }
 
-    fun typeOfEnvase(radioGroup: RadioGroup): String {
-        var idChecked = radioGroup.getCheckedRadioButtonId()
 
-        var tipo = when(idChecked) {
-            radioButtonLata.id -> "Lata"
-            radioButtonBotellin.id -> "Botellin"
-            radioButtonTercio.id -> "Tercio"
-            else -> "Error"
+    // view es el botón clickado de los varios que tienen este onClick
+    fun radioButtonClicked(view: View) {
+        var clickedRB = view as RadioButton
+        when(clickedRB) {
+            radioButtonLata -> selectedEnvase = "Lata"
+            radioButtonTercio -> selectedEnvase = "Tercio"
+            radioButtonBotellin -> selectedEnvase = "Botellin"
+            else -> selectedEnvase = "Error al elegir el envase"
         }
 
-        return tipo
+
+
     }
 
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        println("Nothing selected")
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        println("Selected")
+    }
 
 
 
