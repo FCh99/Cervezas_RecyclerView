c0eb61a79b9f639239c934fca589e10aeeb034bb
Fausto Checa <fausto.checa@gmail.com>
2019-03-13 12:22:31 +0100

    Sec

---
+++ app/src/main/java/udemy/fausto/com/Cervezas_RecyclerView/DataService.kt
@@ -0,0 +1,34 @@
+package udemy.fausto.com.Cervezas_RecyclerView
+
+class DataService {
+
+    var cerveza2 = arrayListOf<Cerveza>(
+        Cerveza(1, 2.00, "Mahou", "Mahou", "1/1/2004", "Si", "Tercio"),
+        Cerveza(2, 3.00, "Coronita", "Coronita", "1/1/2005", "No", "Botellin"),
+        Cerveza(3, 4.00, "Estrella de Galicia", "Estrella de Galicia", "1/1/2006", "Si", "Lata"),
+        Cerveza(4, 5.00, "Woll-Damn", "Woll-Damn", "1/1/2007", "No", "Tercio"),
+        Cerveza(5, 4.00, "Cruzcampo", "Cruzcampo", "1/2/2006", "Si", "Lata"),
+        Cerveza(6, 5.00, "San Miguel", "San Miguel", "1/3/2007", "No", "Tercio"),
+        Cerveza(1, 2.00, "Mahou", "Mahou", "1/1/2004", "Si", "Tercio"),
+        Cerveza(2, 3.00, "Coronita", "Coronita", "1/1/2005", "No", "Botellin"),
+        Cerveza(3, 4.00, "Estrella de Galicia", "Estrella de Galicia", "1/1/2006", "Si", "Lata"),
+        Cerveza(4, 5.00, "Woll-Damn", "Woll-Damn", "1/1/2007", "No", "Tercio"),
+        Cerveza(5, 4.00, "Cruzcampo", "Cruzcampo", "1/2/2006", "Si", "Lata"),
+        Cerveza(6, 5.00, "San Miguel", "San Miguel", "1/3/2007", "No", "Tercio"),
+        Cerveza(1, 2.00, "Mahou", "Mahou", "1/1/2004", "Si", "Tercio"),
+        Cerveza(2, 3.00, "Coronita", "Coronita", "1/1/2005", "No", "Botellin"),
+        Cerveza(3, 4.00, "Estrella de Galicia", "Estrella de Galicia", "1/1/2006", "Si", "Lata"),
+        Cerveza(4, 5.00, "Woll-Damn", "Woll-Damn", "1/1/2007", "No", "Tercio"),
+        Cerveza(5, 4.00, "Cruzcampo", "Cruzcampo", "1/2/2006", "Si", "Lata"),
+        Cerveza(6, 5.00, "San Miguel", "San Miguel", "1/3/2007", "No", "Tercio"),
+        Cerveza(1, 2.00, "Mahou", "Mahou", "1/1/2004", "Si", "Tercio"),
+        Cerveza(2, 3.00, "Coronita", "Coronita", "1/1/2005", "No", "Botellin"),
+        Cerveza(3, 4.00, "Estrella de Galicia", "Estrella de Galicia", "1/1/2006", "Si", "Lata"),
+        Cerveza(4, 5.00, "Woll-Damn", "Woll-Damn", "1/1/2007", "No", "Tercio"),
+        Cerveza(5, 4.00, "Cruzcampo", "Cruzcampo", "1/2/2006", "Si", "Lata"),
+        Cerveza(6, 5.00, "San Miguel", "San Miguel", "1/3/2007", "No", "Tercio")
+
+    )
+
+
+}
