9998280a929ca887698d012fc8b5c25a827bc99d
Fausto Checa <fausto.checa@gmail.com>
2019-03-18 23:22:19 +0100

    nextDay

--- app/src/main/java/udemy/fausto/com/Cervezas_RecyclerView/Activity/ListActivity.kt
+++ app/src/main/java/udemy/fausto/com/Cervezas_RecyclerView/Activity/ListActivity.kt
@@ -1,8 +1,14 @@
 package udemy.fausto.com.Cervezas_RecyclerView.Activity
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_list.*
 import udemy.fausto.com.Cervezas_RecyclerView.Cerveza
 import udemy.fausto.com.Cervezas_RecyclerView.CervezaAdapter
@@ -19,6 +25,17 @@ class ListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+
+        setSupportActionBar(toolbar)
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar()
+        }
+
+
+
+        // ---------
 
         misCervezas = DataService().cerveza2
 
@@ -39,6 +56,30 @@ class ListActivity : AppCompatActivity() {
         var linearLayoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = linearLayoutManager
 
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item!!.getItemId()
+
+        if (id == R.id.list) {
+            Toast.makeText(this, "List", Toast.LENGTH_LONG).show()
+            return true
+        }
+        if (id == R.id.add) {
+            Toast.makeText(this, "Add", Toast.LENGTH_LONG).show()
+            return true
+        } else {
+            Toast.makeText(this, "Error ????", Toast.LENGTH_LONG).show()
+
+        }
+
+        return super.onOptionsItemSelected(item)
 
     }
 }
